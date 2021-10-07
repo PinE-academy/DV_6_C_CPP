b254e0ee8c472dcd4bdc6d2089e4c95bb03895a2
sachhidanandjha <sachiddanandjha@gmail.com>
2021-10-07 19:53:07 +0530

    Create b_array.c

---
+++ Sachhidanand/bit_array/b_array.c
@@ -0,0 +1,44 @@
+//Definition of functions
+typedef struct 
+{
+    unsigned char *array;
+    unsigned int size;
+
+}b_array;
+
+b_array *b_create(size_t size){
+
+    b_array *arr;
+
+    if(size==0)
+    {
+        printf("Can allow an empty array!");
+        return NULL;
+    }
+
+    arr=(b_array *)malloc(sizeof(b_array));
+
+    if (arr==NULL)
+    {
+        printf("Error! Try Again");
+    }
+
+    else
+    {
+        arr->size = size;
+
+        arr->array=(unsigned char *)malloc(sizeof(unsigned char)*(((size/8)+1)));     //To get the memory for the number of character bytes we need 
+
+        if(arr->array==NULL)
+        {
+            printf("Error!");
+            free(arr);
+            arr = NULL;
+        }
+
+    return arr;
+
+    }  
+}
+
+
