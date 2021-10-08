5ec44816aecd0f435d991c935da2ecfdd654935a
sachhidanandjha <sachiddanandjha@gmail.com>
2021-10-08 14:14:42 +0530

    Added: b_destroy()

--- Sachhidanand/bit_array/b_array.c
+++ Sachhidanand/bit_array/b_array.c
@@ -10,12 +10,14 @@ b_array *b_create(size_t size){
 
     b_array *arr;
 
-    if(size==0)
+    //Check if size is 0 or not.
+    if(size==0) 
     {
-        printf("Can allow an empty array!");
+        printf("Can't allow an empty array!");
         return NULL;
     }
 
+    //Dynamically allocating memory.
     arr=(b_array *)malloc(sizeof(b_array));
 
     if (arr==NULL)
@@ -28,18 +30,27 @@ b_array *b_create(size_t size){
     {
         arr->bit = size;
 
-        arr->array=(unsigned char *)malloc(sizeof(unsigned char)*(((size/8)+1)));     //To get the memory for the number of character bytes we need 
+        arr->array=(unsigned char *)malloc(sizeof(unsigned char)*(((size/8)+1)));    //To get the memory for the number of character bytes we need 
 
         if(arr->array==NULL)
         {
             printf("Error!");
-            free(arr);
+            free(arr);          //Free the allocated memory if we dont get the memory for bits.
             arr = NULL;
         }
 
     return arr;
 
     }  
+}
+
+void b_destroy(b_array *arr){
+
+    if (arr->array!=NULL)
+        free(arr->array);   
+    if (arr!=NULL)      
+        free(arr);
+            
 }
 
 
