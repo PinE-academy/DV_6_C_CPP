a19fafb3eda6101ba3b2e927d4c3acdbb2d8073e
uddeshyakumar08 <90714031+uddeshyakumar08@users.noreply.github.com>
2021-10-10 23:27:29 +0530

    Update main.c

--- uddeshya/wcomp/main.c
+++ uddeshya/wcomp/main.c
@@ -3,7 +3,34 @@
 #include"pattern.h"
 #include"strict.h"
 #include"fuzzy.h"
-void main()
+void main(int argc , const char argv[])
 {
-  
+  char *scheme,*word;
+  scheme = argv[4];
+  word=argv[5];
+  FILE *fp;
+  fp=fopen("file.txt","r");//confirm file input
+  if(scheme == "strict")
+  {
+    strict(fp,word);
+  }
+  else
+  {
+    if(scheme=="pattern")
+    {
+      pattern(fp,word);
+    }
+    else
+    {
+      if(scheme=="fuzzy");
+      {
+        fuzzy(fp,word);
+      }
+      else
+      {
+        printf("Wrong scheme");
+      }
+    }
+    
+  }
 }
