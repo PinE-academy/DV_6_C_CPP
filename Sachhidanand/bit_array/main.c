28ad3ccd6d9dfcc7ef8001817526ac604a98be49
sachhidanandjha <sachiddanandjha@gmail.com>
2021-10-11 15:02:43 +0530

    Added: main.c

---
+++ Sachhidanand/bit_array/main.c
@@ -0,0 +1,23 @@
+#include<stdio.h>
+#include<stdlib.h>
+#include<string.h>
+#include"b_array.c"
+#include"b_array.h"
+// #include<limits.h>
+int main(){
+size_t h=24;
+
+    b_array *b = b_create(h);
+
+    for (size_t i = 0; i <(h/8); i++)   //To see the initial value.
+    {
+        printf("%d\n",b->array[i]);
+    }
+   
+
+    for (size_t i = 0; i <(h/8); i++)
+    {
+        printf("%d ",b->array[i]=63*(i+1));
+    }
+    
+}  
