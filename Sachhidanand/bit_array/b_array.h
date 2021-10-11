96dbe736785c88d4bf811123132e6b8005afe5d3
sachhidanandjha <sachiddanandjha@gmail.com>
2021-10-11 16:59:52 +0530

    Modified: Moved struct to b_array.h

--- Sachhidanand/bit_array/b_array.h
+++ Sachhidanand/bit_array/b_array.h
@@ -1,5 +1,12 @@
 //Declaration of functions
 
 // b_create function
+typedef struct 
+{
+    unsigned char *array;
+    unsigned int size;
+
+} b_array;
+
 b_array *b_create(size_t );
 void b_destroy(b_array *);
