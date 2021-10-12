f6262e5dda6fd53a78f3713ef922b9567f2aef63
uddeshyakumar08 <90714031+uddeshyakumar08@users.noreply.github.com>
2021-10-12 14:10:58 +0530

    Update strict.c

--- uddeshya/wcomp/strict.c
+++ uddeshya/wcomp/strict.c
@@ -1,11 +1,19 @@
 #include<stdio.h>
 #include<string.h>
-void strict(FILE *fp, char *word)
+#include<stdlib.h>
+void strict(FILE *f, char *wrd)
 {
-  char str[100];
-  while(*f!=EOF)
+  char str[100],ch;
+  win
+  if(f==NULL)
+    exit(0);
+  else
   {
-    
+  while(f!=EOF)
+  {
+    fgets(str,100,f);
+    fscanf(str,%s,
+  }
   }
   
 }
