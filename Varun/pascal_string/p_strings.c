92f29f67fd92440c61ea2988741b2bb7a6ff1dc0
sharmavarun229414 <sharmavarun229414@gmail.com>
2021-10-14 01:00:22 +0530

    String_create v0.1
    1st code with p_string create.
    p_string object Takes size of string as an input and returns p_string.

---
+++ Varun/pascal_string/p_strings.c
@@ -0,0 +1,21 @@
+#include "p_strings.h"
+#include <stdlib.h>
+#ifndef P_strings
+#define P_strings
+
+struct p_strings *p_create(size_t p_size)
+{
+    struct p_strings *dynamic = malloc(sizeof(struct p_strings));
+    if (dynamic == NULL)
+        return NULL;
+    char *charact = malloc(p_size);
+    if (charact == NULL)
+    {
+        free(dynamic);
+        return NULL;
+    }
+    dynamic->size = p_size;
+    dynamic->str = charact;
+    return dynamic;
+};
+#endif
