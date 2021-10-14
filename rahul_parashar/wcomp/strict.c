bc37b72ea2c71a4f7cc10037b950fb50344a2ca7
Rahul Parashar <91074003+Rahulparashar12@users.noreply.github.com>
2021-10-14 23:51:44 +0530

    Update strict.c

--- rahul_parashar/wcomp/strict.c
+++ rahul_parashar/wcomp/strict.c
@@ -1,1 +1,45 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include "strict.h"
 
+
+void strict_search(char *filename, char *str)
+{
+    
+    FILE *fptr1;
+    fptr1 = fopen("fptr1","w");
+    char s[10],c;
+    int len = strlen(str);
+    int i = 0;
+    int d;
+    int seek = fseek(fptr1, 0, 0);
+    c = fgetc(fptr1);
+    while (c != EOF)
+    {
+        if (c == ' ' || c == '\n')
+        {
+            s[i] = '\0';
+            i = 0;
+            if (strcmp(s, str) == 0)
+            {
+                while (c = fgetc(fptr1) != '\n')
+                {
+                    fseek(fptr1, -2L, 1);
+                    d = ftell(fptr1);
+                }
+                while ((c = fgetc(fptr1)) != '\n')
+                {
+                    fputc(c, fptr1);
+                }
+            }
+        }
+        else
+        {
+            s[i] = c;
+            i++;
+        }
+        c = fgetc(fptr1);
+    }
+fclose(fptr1);
+}
