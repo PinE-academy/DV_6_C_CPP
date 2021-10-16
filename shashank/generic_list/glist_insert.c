80413bd1644c3d73753b87c18ef45f849a2b36c5
Shashank Singh Bisen <shashanksbisen@outlook.com>
2021-10-16 11:20:38 +0530

    Create glist_insert.c

---
+++ shashank/generic_list/glist_insert.c
@@ -0,0 +1,9 @@
+void gList_insert(data, size)
+{
+    int i;
+    for (i=0; i<size; i++)
+    {
+        // traversal to the node where data is to be inserted.
+    }
+    ->data = data;// put the value in the node.
+}
