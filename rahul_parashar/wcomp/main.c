bf3fda398ef97d19f86aedebeebf89f5906f98f3
Rahul Parashar <91074003+Rahulparashar12@users.noreply.github.com>
2021-10-16 18:02:36 +0530

    Update main.c

--- rahul_parashar/wcomp/main.c
+++ rahul_parashar/wcomp/main.c
@@ -1,1 +1,24 @@
+#include<stdio.h>
+#include<string.h>
+#include"strict.h"
+
+int main(int argc, char *argv[])
+{
+     if (argc > 1 && argc < 7)
+    {
+        if ((!(strcmp(argv[2],"-f"))) && (!(strcmp(argv[4],"-s"))))
+        {
+            char *fptr1 = argv[3];
+            char *word = argv[6];
+
+            if (!(strcmp(argv[5],"strict")))
+            {
+                // call to strict
+                strict_search(fptr1, word);
+            }
+            
+        }
+    }
+        return 0;
+}
 
