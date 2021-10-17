7a6d1beaa6b5b89a50fcc13fdbc3d4fc1d4cbb2f
Anusssshhhka <90749058+Anusssshhhka@users.noreply.github.com>
2021-10-17 16:05:08 +0530

    Update g_list.c

--- Anushka/genric_list/g_list.c
+++ Anushka/genric_list/g_list.c
@@ -1,1 +1,13 @@
-
+typedef Struct{
+  void*data;
+  Struct glist*next;
+}glist;
+glist*new_glist()
+{
+  Struct node*head=null;
+     head= (Struct glist*)malloc(sizeof(stuct glist));
+  head-> data=0;
+  head-> next= null;
+  return head;
+}
+    
