ea0d06b0339c272f121e626c9704a006e9ea849b
sharmavarun229414 <87062910+sharmavarun229414@users.noreply.github.com>
2021-10-23 22:31:09 +0530

    p_strings.c v1.1
    Added String concatenation & String Copy Function

--- Varun/pascal_string/p_strings.c
+++ Varun/pascal_string/p_strings.c
@@ -18,18 +18,21 @@ struct p_strings *p_create(size_t p_size)
     dynamic->str = charact;
     return dynamic;
 };
+
 //p_destroy func
 void p_destroy(struct p_strings* dynamic)
 {
     free(dynamic->str);
     free(dynamic);
 };
+
 //p_read func
 char p_read(const struct  p_strings* dynamic,size_t p_in)
 {
     char p_char = dynamic->str[p_in];
     return p_char;
 };
+
 //p_write function
 void p_write(struct p_strings*dynamic,size_t p_in,char p_char)
 {
@@ -42,6 +45,7 @@ size_t p_strlen(const struct p_strings*dynamic)
     size_t pstr_len = dynamic->size;
     return pstr_len;
 };
+
 //p_strcmp function
 int p_strcmp(const struct p_strings*p_str1, const struct p_strings *p_str2)
 {
@@ -64,5 +68,35 @@ int p_strcmp(const struct p_strings*p_str1, const struct p_strings *p_str2)
         }
     }
     return cmp_value;
+};
+
+//p_strcat function
+struct p_strings* p_strcat(struct p_strings* p_str1,const struct p_strings* p_str2)
+{
+    //struct p_strings* p_concat;
+    int size = p_str1->size + p_str2->size;
+    int i = p_str1->size, j = 0;
+    p_str1->size = size;
+    while (i<=size) 
+    {
+        p_str1->str[i] = p_str2->str[j];
+        i++;
+        j++;
+    }
+
+    return p_str1;
+};
+
+//p_strcpy function
+struct p_strings* p_strcpy(const struct p_strings*p_str)
+{
+    struct p_strings* p_copy= malloc(sizeof(struct p_strings));
+    p_copy->size=p_str->size;
+    for(int i=0 ; i<p_str->size ; i++)
+    {
+        p_copy->str[i] = p_str->str[i];
+    }
+
+    return p_copy;
 };
 #endif
