c13c1570ad4c0f706eb2942aea586cd0b7a3eb0a
sharmavarun229414 <87062910+sharmavarun229414@users.noreply.github.com>
2021-10-24 19:23:51 +0530

    Updated Driver file

--- Varun/pascal_string/main.c
+++ Varun/pascal_string/main.c
@@ -1,11 +1,28 @@
 #include<stdio.h>
 #include"p_strings.h"
+
 int main()
 {
-    size_t p_str_size;
-    struct p_strings *p_obj;
-    p_obj = p_create(p_str_size);
+    size_t p_str_size = 9;
+    struct p_strings *p_obj = NULL;
+    struct p_strings *p_obj1 = NULL;
+   // struct p_strings *p_obj2;
+    char *str = "Using functions of Header";
+    printf("start\n");
+    p_obj = p_create(p_str_size); //p_create function call
+    printf("pcreate0");
+    char main_ch;
+    int p_strcmp_value = 1;
+    p_str_size = p_strlen(p_obj);
+    main_ch = p_read(p_obj,p_str_size);
+    p_write(p_obj, p_str_size,main_ch);
+    p_str_size = p_strlen(p_obj); 
+    p_strcmp_value = p_strcmp(p_obj,p_obj1);
+    printf("%d",p_strcmp_value);
+    p_obj = p_strcat(p_obj,p_obj1);
+    p_obj1 = p_strcpy(p_obj);
+    p_obj = p_string(str);
+    str =string(p_obj);
     p_destroy(p_obj);
-
     return 0;
 }
